c0ef04772599c35f664b624f1860776f926d7f24
wkias <willicen@qq.com>
2021-01-08 02:48:41 +0800

    更新2.0接口，添加文章抓取和b64图片功能，修改评论和程序调试信息样式

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
-# 知识星球生成 MarkDown 文件
+# 知识星球抓取精华主题生成 MarkDown 文件
 
 > 请谨慎使用，有封号的风险
 
+使用知识星球2.0接口
 ## 安装依赖
 
 ```bash
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 ```
 
 ## 使用方法
 
-> 修改 `main.py` 中的 `cookies` 以及 要爬取的 `group_id`
+修改 `main.py` 中的 `cookie` 以及 要爬取的 `group_id`
+`cookie`可以从开发者工具的请求Headers中获得
+`group_id`直接抄精华主题页面地址中的一串数字
 
 ```python
 class ZsxqSpider(object):
     def __init__(self):
-        self.group_id = "152********812"
-        self.cookies = {
-            "UM_distinctid": "********",
-            "abtest_env": "product",
-            "zsxq_access_token": "********",
-            "sajssdk_2015_cross_new_user": "1",
-            "sensorsdata2015jssdkcross": "********",
+        self.group_id = "************"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Language": "zh-CN,zh;q=0.9,und;q=0.8,en;q=0.7",
+            "cookie": r"*******************"
         }
 ```
 
 直接运行即可。
 
 ```bash
-python3 main.py
+python main.py
 ```
 
 ## 效果
@@ -36,3 +38,4 @@ python3 main.py
 结果输出为 MarkDown 格式，可以通过 Typora 转为 HTML 或 PDF
 
 ![结果](images/images.png)
+![结果](images/images2.png)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import re
 from urllib import parse
 
+import time
+import base64
 import pangu
 import requests
+from tomd import Tomd
+from bs4 import BeautifulSoup as bs
 from loguru import logger
 from pyquery import PyQuery
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -12,54 +16,59 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class ZsxqSpider(object):
     def __init__(self):
-        self.group_id = "152********812"
-        self.cookies = {
-            "UM_distinctid": "********",
-            "abtest_env": "product",
-            "zsxq_access_token": "********",
-            "sensorsdata2015jssdkcross": "********",
-        }
+        self.delay = 0
+        self.b64 = False
+        self.group_id = "*************"
 
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "Accept-Language": "zh-CN,zh;q=0.9,und;q=0.8,en;q=0.7",
+            "cookie": r"*******************"
         }
-        self.base_url = f"https://api.zsxq.com/v1.10/groups/{self.group_id}/topics"
-        self.end_time = "0"
+        self.digests_url = f"https://api.zsxq.com/v2/groups/{self.group_id}/topics/digests"
+        self.index = "0"
         self.latest_time = None
         self.params = {
-            "scope": "all",
-            "count": "20",
-            "end_time": self.end_time,
+            "sort": "by_create_time",
+            "direction": "desc",
+            "count": "30",
+            "index": self.index,
         }
-        self.data = dict()
+        self.response = dict()
         self.md_list = []
         self.count = 0
 
     def crawler(self):
-        self.params["end_time"] = self.end_time
-        response = requests.get(
-            url=self.base_url, headers=self.headers, cookies=self.cookies, verify=False, params=self.params
-        )
-        self.data = response.json()
-        if self.data.get("succeeded"):
-            return True
-        else:
-            logger.error(f"Crawler Group: {self.group_id} error!")
-            return False
+        self.params["index"] = self.index
+        while True:
+            time.sleep(self.delay)
+            self.response = requests.get(
+                url=self.digests_url, headers=self.headers, verify=False, params=self.params
+            ).json()
+            if self.response.get("succeeded"):
+                logger.info(f"列表: {self.group_id} {self.index} 完成")
+                break
+            else:
+                logger.error(f"列表: {self.group_id} {self.index} 错误")
 
     def parse_topics(self):
-        topics = self.data.get("resp_data", {}).get("topics", [])
-        if topics:
-            self.end_time = topics[-1].get("create_time")
-        if self.end_time != self.latest_time:
-            self.latest_time = self.end_time
-        else:
-            logger.info(f"Crawler Group: {self.group_id} finished!")
-            return False
+        topics = self.response.get("resp_data", {}).get("topics", [])
+        self.index = self.response.get("resp_data", {}).get("index")
+        self.index = str(self.index)
         self.count += len(topics)
-        for topic in topics:
+        for t in topics:
+            topic_id = t.get("topic_id")
+            topic_url = f"https://api.zsxq.com/v2/topics/{topic_id}/info"
+            while True:
+                time.sleep(self.delay)
+                response = requests.get(url=topic_url, headers=self.headers).json()
+                if response.get("succeeded"):
+                    topic = response.get("resp_data", {}).get("topic", {})
+                    logger.info(f"主题: {t.get('title')[:15]} 完成")
+                    break
+                else:
+                    logger.error(f"主题: {t.get('title')[:15]} 错误")
             md = []
             topic_type = topic.get("type")
             md.append(self.parse_header(topic))
@@ -73,18 +82,43 @@ class ZsxqSpider(object):
             md.append(self.parse_comment(topic))
             md_text = "\n".join(md)
             self.md_list.append(md_text)
-        logger.info(f"Crawler Group: {self.group_id} {self.count} count!")
+        logger.info(f"{self.group_id} {self.count} 😀😆")
         return True
 
-    @staticmethod
-    def parse_html(content):
+    def parse_file(self, name, id):
+        while True:
+            time.sleep(self.delay)
+            download_url = f"https://api.zsxq.com/v2/files/{id}/download_url"
+            response = requests.get(url=download_url, headers=self.headers).json()
+            if response.get("succeeded"):
+                file_url = response.get("resp_data", {}).get("download_url")
+                break
+            else:
+                logger.error(f"文件: {name} 错误")
+        with open(name, 'w') as f:
+            f.write(requests.get(url=file_url, headers=self.headers))
+    
+    def parse_article(self, url):
+        html = requests.get(url=url, headers=self.headers).text
+        html = bs(html, features="lxml")
+        html = html.find("div", class_="content")
+        md = Tomd(str(html)).markdown
+        md = md.replace("\n", "")
+        return md
+
+    def parse_html(self, content):
         content = content.replace("\n", "<br>")
         result = re.findall(r"<e [^>]*>", content)
         if result:
             for i in result:
                 html = PyQuery(i)
                 if html.attr("type") == "web":
-                    template = "[%s](%s)" % (parse.unquote(html.attr("title")), parse.unquote(html.attr("href")))
+                    title = parse.unquote(html.attr("title"))
+                    url = parse.unquote(html.attr("href"))
+                    template = "[%s](%s)" % (title, url)
+                    template += f"\n## {title}\n"
+                    template += self.parse_article(url)
+                    template += "\n"
                 elif html.attr("type") == "hashtag":
                     template = " `%s` " % parse.unquote(html.attr("title"))
                 elif html.attr("type") == "mention":
@@ -95,19 +129,33 @@ class ZsxqSpider(object):
         else:
             content = pangu.spacing_text(content)
         return content
+    
+    def img2b64(self, img):
+        try:
+            url = img.get("original").get("url")
+        except Exception:
+            url = img.get("large").get("url")
+        if not self.b64:
+            return url
+        type_ = img.get("type")
+        img = requests.get(url=url, headers=self.headers).content
+        b64 = base64.b64encode(img).decode()
+        b64 = "data:image/" + type_ + ";base64," + b64
+        return b64
 
-    @staticmethod
-    def parse_header(topic):
+    def parse_header(self, topic):
         data_time = topic["create_time"]
-        group = topic.get("group", {}).get("name")
-        return f"# {group} - {data_time.split('T')[0]}"
+        # group = topic.get("group", {}).get("name")
+        author = topic.get("talk", {}).get("owner", {}).get("name")
+        return f"# {author} - {data_time.split('T')[0]}"
 
     def parse_comment(self, topic):
         comments = [
             comment.get("owner").get("name") + ": " + self.parse_html(comment.get("text", ""))
             for comment in topic.get("show_comments", [])
         ]
-        comment_text = "```\n" + "\n".join(comments) + "\n```" if comments else ""
+        # comment_text = "```\n" + "\n".join(comments) + "\n```\n\n" if comments else ""
+        comment_text = "> \n" + "\n".join(comments) + "\n\n\n" if comments else ""
         return comment_text
 
     def parse_qa(self, topic):
@@ -116,17 +164,17 @@ class ZsxqSpider(object):
         question_images = []
         if topic.get("question").get("images"):
             for img in topic.get("question").get("images"):
-                url = img.get("large").get("url")
-                question_images.append(url)
+                b64 = self.img2b64(img)
+                question_images.append(b64)
         answer_images = []
         if topic.get("answer").get("images"):
             for img in topic.get("answer").get("images"):
-                url = img.get("large").get("url")
-                answer_images.append(url)
+                b64 = self.img2b64(img)
+                answer_images.append(b64)
 
         question_image_text = "\n".join([f"![]({question_image})" for question_image in question_images])
         answer_image_text = "\n".join([f"![]({answer_image})" for answer_image in answer_images])
-        md = ["## Question:", question_text, question_image_text, "## Answer:", answer_text, answer_image_text]
+        md = ["## 问:", question_text, question_image_text, "## 答:", answer_text, answer_image_text]
         return md
 
     def parse_talk(self, topic):
@@ -134,8 +182,8 @@ class ZsxqSpider(object):
         images = []
         if topic.get("talk").get("images"):
             for img in topic.get("talk").get("images"):
-                url = img.get("large").get("url")
-                images.append(url)
+                b64 = self.img2b64(img)
+                images.append(b64)
 
         image_text = "\n".join([f"![]({image})" for image in images])
         md = [content, image_text]
@@ -143,15 +191,15 @@ class ZsxqSpider(object):
 
     def run(self):
         while True:
-            if not self.crawler():
-                break
-            if not self.parse_topics():
+            self.crawler()
+            self.parse_topics()
+            if self.index == "0":
                 break
         text = "\n".join(self.md_list[::-1])
-        logger.info(f"Writing into {self.group_id}.md")
-        with open(f"data/{self.group_id}.md", "w") as f:
+        logger.info(f"写入 {self.group_id}.md")
+        with open(f"data/{self.group_id}.md", "w", encoding='utf8') as f:
             f.write(text)
-        logger.info(f"Crawler {self.group_id} Finished, Count: {self.count}")
+        logger.info(f"{self.group_id} 抓取完成  {self.count} 主题")
 
 
 if __name__ == "__main__":
